72578ebe1f98d2e670cf530e24cd77fda7b85ffb
Alexander Mizhgorodskiy <120034948+mizhgorodskiyalexander@users.noreply.github.com>
2022-12-08 00:05:55 +0300

    add task3

---
+++ Mizhgorodskiy_CS_Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mizhgorodskiy_CS_Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = true;
+            while (b)
+            {
+                string[] d = Console.ReadLine().Split();
+
+                for (int i = 0; i < d.Length; ++i)
+                {
+                    if (d[i].All(x => Char.IsDigit(x)) && Convert.ToInt32(d[i]) % 3 == 0)
+                    {
+                        Console.WriteLine(d[i]);
+                        b = false;
+                        
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
