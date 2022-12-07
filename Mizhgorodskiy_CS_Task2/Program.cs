0730eb939dfe457b8649a41512faaefacd8bb776
Alexander Mizhgorodskiy <120034948+mizhgorodskiyalexander@users.noreply.github.com>
2022-12-07 23:58:20 +0300

    add task2

---
+++ Mizhgorodskiy_CS_Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mizhgorodskiy_CS_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = true;
+            while (b)
+            {
+                string name = Console.ReadLine();
+
+                if (name == "Вячеслав")
+                    Console.WriteLine("Привет, Вячеслав");
+                else
+                    Console.WriteLine("Нет такого имени");
+            }
+        }
+    }
+}
---
+++ Mizhgorodskiy_CS_Task2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Mizhgorodskiy_CS_Task2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mizhgorodskiy_CS_Task2")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("242fa02f-60e6-468b-a321-d6c63c38973a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
